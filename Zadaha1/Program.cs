693147efc23a3bb8bce5f0764186978a3c380577
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-12 11:12:01 +0300

    Доделали задачу найти элемент в массиве

--- Zadaha1/Program.cs
+++ Zadaha1/Program.cs
@@ -5,7 +5,7 @@ int[] array = {3,2,5,1,6,4,3,8,9};
 
 int n = array.Length;
 
-int find = 4;
+int find = 9;    // Какой элемент в массиве нужно найти
 int index = 0;
 while(index<n)
 {
@@ -13,7 +13,7 @@ while(index<n)
     if (array[index]==find)
     {
         Console.WriteLine(index);
-        break;   //  Останавливает на первом элементе если два одинаковых числа
+       // break;   //  Останавливает на первом элементе если два одинаковых числа
     }
     index++;
 }
