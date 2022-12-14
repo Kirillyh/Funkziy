5903f785d71ec4c248582f10507c0ebeede29bb6
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-14 22:38:39 +0300

    Добавили факториал

---
+++ RECURSIY2/Program.cs
@@ -0,0 +1,7 @@
+﻿int Factorial (int n)     //Метод нахождение факториала    1! =1   0! = 1
+{
+if (n == 1) return 1;    //Если 1 возврашаем  1
+else 
+return n * Factorial(n-1);  //Если не 1 делаем по формуле
+}
+Console.WriteLine(Factorial(3));  //1*2*3=6
