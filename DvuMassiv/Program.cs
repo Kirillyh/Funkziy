8302b692a49e944294dedccda1cb81bf18580e0f
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-14 21:13:59 +0300

    Добавили двух мерные массивы

---
+++ DvuMassiv/Program.cs
@@ -0,0 +1,11 @@
+﻿int[,] matrix = new int[3, 4];    // Задаем двумерный массив
+
+
+for (int i = 0; i < matrix.GetLength(0); i++)     // Внешний цикл клацаюший строки   for (int i = 0; i<3; i++) == matrix.GetLength(0)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) // Внутренний цикл клацаюший столбцы     for (int j = 0; j < 4; j++) == matrix.GetLength(1)
+    {
+        Console.Write($"{matrix[i,j]} ");
+    }
+    Console.WriteLine(); //Переход на новою строку чтобы было красиво
+}
---
+++ DvuMassiv2/Program.cs
@@ -0,0 +1,28 @@
+﻿void PrintArray(int[,]matrix)                            //CОздаем метод из прошлой задачи который выводит таблицу цифр
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)     
+    {
+         for (int j = 0; j < matrix.GetLength(1); j++) 
+        {
+        Console.Write($"{matrix[i,j]} ");
+        }
+    Console.WriteLine(); 
+    }
+
+}
+void FillArray(int[,]matrix)                       //CОздаем метод который будет заполнять наш массив
+{
+     for (int i = 0; i < matrix.GetLength(0); i++) 
+     {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           matrix [i,j]= new Random().Next(1,10); // Заполняем массив рандомными числами
+        }
+     }
+}
+int[,] matrix = new int[3, 4];    // Задаем двумерный массив
+
+PrintArray(matrix);              // ВЫводим в консоль метод PrintArray
+FillArray(matrix);                // 
+Console.WriteLine();              // Делаем пробел
+PrintArray(matrix);              //   Выводим заполненый рандомными числами метод
