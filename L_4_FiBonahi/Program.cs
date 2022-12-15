e7a6d2d49ae2f00867573f6eb33835438259372a
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-12-15 21:23:50 +0300

    Добавили рекурсию

---
+++ L_4_FiBonahi/Program.cs
@@ -0,0 +1,13 @@
+﻿//   f(1) = 1   Задаем числа
+//   f(2) = 1
+//   f(n) = f(n-1) + f(n-2)
+
+int Fibonacci(int n)
+{
+    if(n==1 || n == 2) return 1;   //Если значение n ==1 или n == 2 то возврашаем 1
+    else return Fibonacci(n-1) + Fibonacci(n-2);  // иначе  возврашаем
+}
+for (int i=1; i<10; i++)  // сколько чифр фибоначи
+{
+    Console.WriteLine(Fibonacci(i));
+}
---
+++ L_4_Recursiy/Program.cs
@@ -0,0 +1,10 @@
+﻿double Factorial (double n)     //Метод нахождение факториала    1! =1   0! = 1
+{
+if (n == 1) return 1;    //Если 1 возврашаем  1
+else 
+return n * Factorial(n-1);  //Если не 1 делаем по формуле
+}
+for (double i = 1; i < 40; i++)
+{
+Console.WriteLine($"{i}! = {Factorial(i)}");  
+}
